6a74d18dd3ffc5f6d5c9f8ea0ed8c3d9e3d61bf9
Bikram Gyawali <gyawalibikram0@gmail.com>
2024-10-09 22:15:04 -0800

    Complete Day 2

---
+++ Day 1.c
@@ -0,0 +1,15 @@
+// To print Hello World
+
+#include<stdio.h>
+ 
+ int a=5;
+ 
+ main (){
+  printf("Hello World");
+
+
+ }
+ 
+
+ 
+ 
---
+++ Day 2.c
@@ -0,0 +1,46 @@
+// to print hello
+
+//#include<stdio.h>  //header file
+//main(){
+//char n[1];
+//	/* hii
+//	hello
+//	bye */
+//	printf("Enter the name"); //prompt
+//	scanf("%s",n); //read
+//	printf("The name is %s",n); //display
+//
+//	
+//}
+
+
+// to print Hello World
+/*
+#include<stdio.h>
+main(){
+	printf("Hello World");
+	// to display result and to give prompt to the user
+}
+*/
+
+// to find the area
+ 
+ 
+ #include<stdio.h>
+ main(){
+ 	int l,b,a;
+ 	printf("Enter the length :");
+ 	scanf("%d",&l);
+ 		printf("Enter the Breadth :");
+ 	scanf("%d",&b);
+ 	a=l*b;
+ 	printf("The area is %d",a);
+ 	
+ }
+ 
+
+
+
+
+
+
